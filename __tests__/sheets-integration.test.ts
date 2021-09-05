a2826b96de7dd9f552a13e6702bdc0fa0dd393c4
geewee <gustavwengel@gmail.com>
2021-09-05 14:05:28 +0200

    jest works now

---
+++ __tests__/sheets-integration.test.ts
@@ -0,0 +1,8 @@
+import { google, sheets_v4 } from 'googleapis';
+import _ from 'lodash';
+
+describe('Sheets integration integration tests', () => {
+    it('Should be able to read from public sheet', () => {
+        console.log("Does this work")
+    });
+})
