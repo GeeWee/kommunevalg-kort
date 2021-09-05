21cffe5bb339bee9c8844fcc3e0384b5134a5412
Anna Mie Hansen <anna.m.hansen@hotmail.com>
2021-09-05 15:20:41 +0200

    Added get req for google sheet

--- __tests__/sheets-integration.test.ts
+++ __tests__/sheets-integration.test.ts
@@ -1,8 +1,19 @@
+import axios from 'axios';
 import { google, sheets_v4 } from 'googleapis';
 import _ from 'lodash';
 
+interface Begivenhed {
+    Tidspunkt  : string,
+    Begivenhed : string,
+    Kommune    : string,
+    Sted       : string,
+    Info       : string
+}
+
 describe('Sheets integration integration tests', () => {
-    it('Should be able to read from public sheet', () => {
-        console.log("Does this work")
+    it('Should be able to read from public sheet', async () => {
+        const values = await axios.get<Begivenhed[]>("https://script.google.com/macros/s/AKfycbzboDHSRLhg7ILtaPq9u_cZLm-isnLD0O913RX74hvicF05wLgHVzKBrnNHptW8zio_/exec?path=Ark1")
+        console.log(values.data)
+        console.log("Done -")
     });
 })
