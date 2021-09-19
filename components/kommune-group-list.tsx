6008fb7c3302f72460155f97cbd44881c95ed549
geewee <gustavwengel@gmail.com>
2021-09-19 13:47:30 +0200

    empty state for group list

--- components/kommune-group-list.tsx
+++ components/kommune-group-list.tsx
@@ -9,6 +9,16 @@ export interface KommuneGroupList {
 }
 
 export const KommuneGroupList: FunctionComponent<KommuneGroupList> = (props) => {
+    // Empty state for groups
+    if (props.groups.length === 0){
+        return <div>
+            Vi har pt. ikke information om nogle lokale grupper i din kommune.
+            Hvis du er med i en gruppe som ikke står her, eller du gerne vil være med til at starte en gruppe så kan du <a
+            href="https://www.klimabevaegelsen.dk/kommunalvalg">tilmelde dig Klimabevægelsens Kampagnehold</a>
+        </div>
+    }
+
+
     const rows = props.groups.map((group, index) => {
 
         return <tr key={index}>
--- stories/kommune-group-list.stories.tsx
+++ stories/kommune-group-list.stories.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
 
-import { KommuneGroupList } from '../components/kommune-group-list';
+import {KommuneGroupList} from '../components/kommune-group-list';
 
 export default {
-  title: 'KommuneGroupList',
-  component: KommuneGroupList,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: 'KommuneGroupList',
+    component: KommuneGroupList,
+    argTypes: {
+        backgroundColor: {control: 'color'},
+    },
 } as ComponentMeta<typeof KommuneGroupList>;
 
 const Template: ComponentStory<typeof KommuneGroupList> = (args) => <KommuneGroupList {...args} />;
 
+export const EmptyState = Template.bind({});
+EmptyState.args = {
+    groups: []
+};
+
 export const Primary = Template.bind({});
 Primary.args = {
-  // insert props here
+    groups: [{
+        description: "description",
+        groupName: "groupName",
+        kommune: "Aalborg",
+        location: "location",
+        meetings: "meetings",
+        person: "person"
+    }]
 };
