8ffe046ce400b3265dd42aadf603fce94445b24b
geewee <gustavwengel@gmail.com>
2021-09-22 20:49:37 +0200

    Iframe fix

--- components/sign-up-link.tsx
+++ components/sign-up-link.tsx
@@ -6,26 +6,27 @@ export interface SignUpLinkProps {
 
 export const SignUpLink: React.FC<SignUpLinkProps> = (props) => {
 
-        // If we're in an iframe, we scroll to the top of our parent
-        if (inIframe()) {
-            console.log('INSIDE IFRAME??');
-            const onClick = () => {
-                window.parent.postMessage("scrollToTop", "http://localhost:63342/");
-            }
+        // TODO for some reason this doesn't work properly on all links
+        // // If we're in an iframe, we scroll to the top of our parent
+        // if (inIframe()) {
+        //     console.log('INSIDE IFRAME??');
+        //     const onClick = () => {
+        //         window.parent.postMessage("scrollToTop", "http://localhost:63342/");
+        //     }
+        //
+        //     return (
+        //         <a href="javascript:;" onClick={onClick}>
+        //             {props.children}
+        //         </a>
+        //     )
+        //
+        // }
+        return (
+            <a href="https://www.klimabevaegelsen.dk/kommunalvalg" target="_top">
+                {props.children}
+            </a>
+        )
 
-            return (
-                <a href="javascript:;" onClick={onClick}>
-                    {props.children}
-                </a>
-            )
-
-        } else {
-            return (
-                <a href="https://www.klimabevaegelsen.dk/kommunalvalg" target="_top">
-                    {props.children}
-                </a>
-            )
-        }
 
     }
 ;
